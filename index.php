3b017fbc78ca449524bc4e3e9ba197a8ee610162
BruunoA <bandia@inscaparrella.cat>
2024-10-05 14:52:10 +0200

    prueba v1

---
+++ index.php
@@ -0,0 +1,15 @@
+<?php
+    echo "    <h1>hola esto es una prueba de servidor</h1>";
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>  
+    <h1>esto para prueba</h1>
+</body>
+</html>
